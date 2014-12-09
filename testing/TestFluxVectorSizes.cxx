a724bba69d72e5bfe9183190b40bd87bf38d733c
Bertrand Moreau <bertrand.moreau@thecosmocompany.com>
2014-12-09 16:44:46 +0100

    Issue #4 - Flux format refactoring
    - added TestFluxVectorSizes test

---
+++ testing/TestFluxVectorSizes.cxx
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <assert.h>
+#include "fbcFBAProblem.hxx"
+
+/** \brief Test the size of the resulting vectors of fluxes after resolution.
+ * Loads an SBML model, solves it and chack the size of its resulting vector
+ * of fluxes and exchange fluxes.
+ */
+int main (int argc, char *argv[])
+{
+  fbc::FBAProblem pbm;
+  pbm.initFromSBMLFile(argv[1]);
+  pbm.solveProblem();
+  assert(pbm.getSolution()->getFluxes()->size() == 2546);
+  assert(pbm.getSolution()->getExchangeFluxes()->size() == 345);
+  return 0;
+}
